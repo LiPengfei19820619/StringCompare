c25959022089a1de8a19b304ee9b402ae286a7d8
LiPengfei19820619 <22201846@qq.com>
2018-03-08 01:12:15 +0800

    增加测试用例:字符和数字混合的两个字符串的比较

--- test/case/test.cpp
+++ test/case/test.cpp
@@ -14,3 +14,10 @@ TEST(StringCompare, GIVEN_Two_Strings_With_Only_Digits_WHEN_Compare_THEN_Compare
 {
 	EXPECT_EQ(StringCompare("92", "256"), -1);
 }
+
+// 两个字符数字混合的字符串比较
+TEST(StringCompare, GIVEN_Two_Strings_With_Digits_And_Chars_WHEN_Compare_THEN_GetResult)
+{
+	EXPECT_EQ(StringCompare("abc123", "abd90"), -1);
+	EXPECT_EQ(StringCompare("abc123", "abc90"), 1);
+}
